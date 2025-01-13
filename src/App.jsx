6bdb18d28314bf555ef33f917cf2435f124b7038
Ramla-Razykan <ramla.eman.ra@gmail.com>
2025-01-13 13:26:19 +0500

    set responsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <div className='bg-orange-300 p-6 rounded-lg text-orange-700'>
-        <h1 className='text-[4rem]'>Hello Mirror Input</h1>
+        <h1 className='lg:text-[4rem] text-[1.5rem]'>Hello Mirror Input</h1>
         <input type="text" placeholder='Write Something' value={inputValue} onChange={(e) => setInputValue(e.target.value)} className='py-1 text-[1.3rem] px-3 rounded-md'/>
         <p className='text-[1.6rem] '>Your Text Is: <span className='text-[1.4rem] text-orange-600'>{inputValue}</span></p>
       </div>
